77c836f18d442832d8542fdc5d3d544e4d654e90
nikhili2it <nikhil98013@gmail.com>
2026-09-05 18:31:37 +0530

    comparison operations
    Comparison b/w diff. datatypes and their result explanation.

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1); //converted automatically
+// console.log("02" > 1);
+
+
+// console.log(null > 0); //false
+// console.log(null==0); //false
+// console.log(null>= 0); //true coz null is treated as 0 here for comparision.
+
+// console.log(undefined > 0); // undefined always gives false
+// console.log(undefined==0); //false
+// console.log(undefined>= 0); //false
+
+// === (strict check)
+
+console.log(2==="2"); // false due to type mismatch.

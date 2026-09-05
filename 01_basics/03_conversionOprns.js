93951b57472636f94c2431c7c05bab39cc3e97dd
nikhili2it <nikhil98013@gmail.com>
2026-09-05 17:25:03 +0530

    Replace 03_conv file

--- 01_basics/03_conversionOprns.js
+++ 01_basics/03_conversionOprns.js
@@ -1,12 +1,12 @@
 let score = undefined // Used booleanValue,Null,33,string(both numeral and alphabets)  as well
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,16 +15,55 @@ console.log(valueInNumber);
 let isLoggedIn = "nikhil"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn)
 
 // 1 => true
 // "" => false
 // "nikhil" => true
 
 let someNumber = 33
-let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//--------------------Operations---------------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+
+// console.log(1+2+"2"); //32
+
+// console.log(2+"2"+5); //225
+
+// console.log(+true); //1
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2 // very bad code writing style
+
+// let gameCounter = 100
+// ++gameCounter
+
+// console.log(gameCounter); //101 for both ++gC or gC++
+// Study prefix and postfix increment from mdn docs
+
 

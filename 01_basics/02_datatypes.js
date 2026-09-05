a08dce1107609de011cee0246a7e0bc8b5d346ba
nikhili2it <nikhil98013@gmail.com>
2026-09-05 17:04:39 +0530

    datatypes and conversion files uploaded
    Datatypes file replaced and conversion file was uploaded

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+//code readability must be better.
+// WRONG PRACTICE console.log(3+3); console.log("Hello")
+
+//Correct practice
+console.log(3+3);
+
+console.log("Hello")
+
+let name = "nikhil"
+let age = 19
+let isLoggedIn = false
+
+// number=> 2 raised to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => (for uniquness)
+
+// object
+
+console.log(typeof age);//number
+
+console.log(typeof null);//Object
+
+console.log(typeof undefined);//Undefined
+
---
+++ 01_basics/03_conversionOprns.js
@@ -0,0 +1,30 @@
+let score = undefined // Used booleanValue,Null,33,string(both numeral and alphabets)  as well
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true/false => 1/0
+
+let isLoggedIn = "nikhil"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// "" => false
+// "nikhil" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
